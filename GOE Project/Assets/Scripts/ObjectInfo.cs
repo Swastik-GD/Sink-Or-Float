fdcb754cdafe1f6433c07f3fde55aa196aa141b5
Swastik-GD <1804swastikpatel@gmail.com>
2025-11-28 15:23:52 +0530

    new project

---
+++ GOE Project/Assets/Scripts/ObjectInfo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectInfo : MonoBehaviour
+{
+    [Header("Info")]
+    public string objectName;
+    public bool willFloat;
+
+    [Header("Buttons")]
+    public Button sinkButton;
+    public Button floatButton;
+
+    [Header("Targets")]
+    public Transform sinkTarget;
+    public Transform floatTarget;
+
+
+    public void Start()
+    {
+        
+    }
+
+}
---
+++ GOE Project/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpaawner : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> Spawner1;
+
+
+}
